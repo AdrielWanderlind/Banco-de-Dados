af0512228a42a594e45e51e53888f472e06f983e
Adriel Wanderlind <adrielwanderlind80@gmail.com>
2026-08-26 09:37:38 -0300

    Finalização alter table e inserts e selects

--- aula2.sql
+++ aula2.sql
@@ -61,18 +61,53 @@ create table pedido(
     status varchar(20)
 );
 
-alter table categoria add constraint categoria_nome_uq unique(nome);
+create table itempedido(
+	pedido_id int not null,
+    produto_id int not null,
+    quantidade_id int not null,
+    preco_unit decimal(3,2) default 0
+);
+
+-- Primary keys
 alter table fornecedor add constraint fornecedor_idforn_pk primary key(idforn);
-alter table fornecedor add constraint fornecedor_cnpj_uq unique(cnpj);
 alter table produto add constraint produto_id_pk primary key(id);
+alter table cliente add constraint cliente_id_pk primary key(idcliente);
+alter table pedido add constraint pedido_id_pk primary key(id);
+alter table itempedido add constraint itempedido_pk primary key(pedido_id, produto_id);
+
+-- Uniques
+alter table categoria add constraint categoria_nome_uq unique(nome);
+alter table fornecedor add constraint fornecedor_cnpj_uq unique(cnpj);
+alter table cliente add constraint cliente_email_uq unique(email);
+
+-- Checks
 alter table produto add constraint produto_estoque_ck check(estoque>0);
 alter table produto add constraint produto_preco_ck check(preco>0);
+alter table pedido add constraint pedido_status_ck check (status in('Em andamento', 'Cancelado', 'Concluido'));
+alter table cliente add constraint cliente_status_ck check(status in('Ativo','Inativo'));
+alter table itempedido add constraint itempedido_preco_ck check(preco_unit>0);
+
+
+-- Foreign keys
 alter table produto add constraint produto_idforn_fk foreign key(idforn) references fornecedor(idforn);
 alter table produto add constraint produto_idcategoria_fk foreign key(idcategoria) references categoria(idcategoria);
-alter table cliente add constraint cliente_id_pk primary key(idcliente);
-alter table cliente add constraint cliente_email_uq unique(email);
-alter table cliente add constraint cliente_status_ck check(status in('Ativo','Inativo'));
-alter table pedido add constraint pedido_id_pk primary key(id);
+alter table pedido add constraint cliente_pedido_fk foreign key(cliente_id) references cliente(idcliente);
+alter table itempedido add constraint pedido_itempedido_fk foreign key (pedido_id) references pedido(id);
+alter table itempedido add constraint produto_itempedido_fk foreign key (produto_id) references produto(id);
+
+-- 1 Para N / Um cliente pode ter mais de 1 pedido, mas cada pedido só pode ter 1 cliente
 alter table pedido add constraint pedido_clienteid_fk foreign key(cliente_id) references cliente(idcliente);
+
+-- Default status 
 alter table pedido modify column status varchar(20) default 'Em Andamento';
-alter table pedido add constraint pedido_status_ck check (status in('Em andamento', 'Cancelado', 'Concluido'));
+
+-- Inserts
+
+insert into categoria(idcategoria,nome)
+values (2,'Higiene');
+insert into categoria(idcategoria,nome)
+values (1,'Comida');
+
+
+select * from categoria
+order by idcategoria desc;
